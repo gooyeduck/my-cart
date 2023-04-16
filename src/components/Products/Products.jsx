ac62246e226163745f3a5eea5405548057bcff37
cupid chakma <greendaycu20@gmail.com>
2023-04-16 22:26:09 +0600

    updated: products css

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import img from '../../assets/dummy-product.png';
+import { FaPlusCircle, FaMinusCircle } from 'react-icons/fa';
 import './_products.scss';
 
 const Products = () => {
@@ -9,22 +10,34 @@ const Products = () => {
                 <img src={img}/>
                 <h5>Product 1</h5>
                 <p>$25</p>
-                <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis cursus nibh, ac finibus leo. Pellentesque semper sed est consectetur facilisis.</article> 
-                <button>Add To Cart</button>
+                <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis cursus nibh, ac finibus leo. Pellentesque semper sed est consectetur facilisis.</article>
+                <button className="product-button-group">
+                    {<FaPlusCircle/>}
+                    <input type="text" name="product_number"/>
+                    {<FaMinusCircle/>}
+                </button> 
             </li>
             <li className="product-details">
                 <img src={img}/>
                 <h5>Product 2</h5>
                 <p>$25</p>
                 <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis cursus nibh, ac finibus leo. Pellentesque semper sed est consectetur facilisis.</article> 
-                <button>Add To Cart</button>
+                <button className="product-button-group">
+                    {<FaPlusCircle/>}
+                    <input type="text" name="product_number"/>
+                    {<FaMinusCircle/>}
+                </button>
             </li>
             <li className="product-details">
                 <img src={img}/>
                 <h5>Product 3</h5>
                 <p>$25</p>
                 <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis cursus nibh, ac finibus leo. Pellentesque semper sed est consectetur facilisis.</article> 
-                <button>Add To Cart</button>
+                <button className="product-button-group">
+                    {<FaPlusCircle/>}
+                    <input type="text" name="product_number"/>
+                    {<FaMinusCircle/>}
+                </button>            
             </li>
         </ul>
     )
